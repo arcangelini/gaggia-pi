5ade0d387552fa647a39efed5c43f6849821ee01
Tony Arcangelini <33258733+arcangelini@users.noreply.github.com>
2021-02-08 17:00:09 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+/**
+ * Setting up the server. Following these guides for help
+ * https://developer.wordpress.org/coding-standards/wordpress-coding-standards/javascript/
+ * https://www.javascriptjanuary.com/blog/an-introduction-to-iot-internet-of-toilets
+ * https://medium.com/sysf/introduction-to-iot-with-raspberry-pi-and-node-js-using-rgb-led-lights-77f4750a5ea9
+ *
+ */
+
+const express = require( 'express' )
+const socket = require( 'socket.io' )
+const app = express()
+const port  = 9000
+
+app.get( '/', ( request, response ) => {
+    response.sendFile( '/home/pi/gaggia/public/index.html' ), {
+        headers: {
+            'Content-Type': 'text/html',
+        },
+    };
+});
+
+app.use( '/assets', express.static( '/home/pi/gaggia/public/assets' ) );
+app.use( '/assets', express.static( '/home/pi/gaggia/node_modules/socket.io-client/dist' ) );
+
+
+const server = app.listen( port, () => {
+    console.log( 'Express server started!' );
+});
+const io = socket( server, {
+    cors: {
+        origin: '*',
+    }
+})
+
+io.on( 'connection', ( client ) => {
+    console.log( 'SOCKET: ', 'A client connected', client.id );
+
+    client.on( 'brew', () => {
+        console.log( "Starting brew" );
+    })
+});
