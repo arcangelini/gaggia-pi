6fc70f648e665c0ae2fdcc9645b30802f489afe1
arcangelini <tony@arcangelini.com>
2021-02-10 01:25:06 +0000

    First working scale.py with calibrated number

--- helper/hx711py/scale.py
+++ helper/hx711py/scale.py
@@ -25,13 +25,7 @@ def cleanAndExit():
 hx = HX711(5, 6)
 hx.set_reading_format("MSB", "MSB")
 
-# HOW TO CALCULATE THE REFFERENCE UNIT
-# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-
-hx.set_reference_unit(113)
+hx.set_reference_unit(6943)
 
 hx.reset()
 
@@ -42,7 +36,7 @@ print("Tare done! Add weight now...")
 while True:
     try:
         val = hx.get_weight(5)
-        print(val)
+        print( '{:.1f}'.format(val + 0) )
 
         hx.power_down()
         hx.power_up()
