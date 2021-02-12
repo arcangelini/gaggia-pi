b6d18f9e20dfab0af6e38746624f1fff1cfe9a82
Tony Arcangelini <tony@arcangelini.com>
2021-02-12 01:37:34 +0100

    Trying new HX711 lib

---
+++ helper/hx711/index.d.ts
@@ -0,0 +1,13 @@
+declare class HX711 {
+  constructor(clockPin: number, dataPin: number, gain?: number)
+
+  read(): number
+  setScale(scale: number): void
+  setOffset(offset: number): void
+  tare(times?: number): void
+  getUnits(times?: number): number
+  getOffset(): number
+  getScale(): number
+}
+
+export = HX711
