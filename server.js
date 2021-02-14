919b1d31e925d61d9cedfea3215208b301fce7f8
Tony Arcangelini <tony@arcangelini.com>
2021-02-14 11:15:43 +0100

    update

--- server.js
+++ server.js
@@ -39,15 +39,15 @@ io.on( 'connection', ( client ) => {
     client.emit( "Connected to server" );
     
     client.on( 'brew_start', ( setWeight ) => {
-
+        let brewData = ( data ) => {
+            console.log ( data )
+        }
         const scale = spawn( 'python', [ '/home/pi/gaggia/helper/hx711py/scale.py', setWeight ], {
             timeout: 10000,
             detached: true,
 	    });
 
-        scale.stdout.on( 'data', ( output ) => {
-            console.log( output )
-        })
+        scale.stdout.pipe( brewData() )
 
     })
 
