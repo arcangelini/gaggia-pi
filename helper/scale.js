b729232738d2a8770518646b48069ffef78036ff
Tony Arcangelini <tony@arcangelini.com>
2021-02-14 11:43:13 +0100

    Add scale.js

--- helper/scale.js
+++ helper/scale.js
@@ -6,8 +6,5 @@ const setWeight = process.argv[0]
 
 const scale = spawn( 'python', [ '/home/pi/gaggia/helper/hx711py/scale.py', setWeight ], {
     timeout: 10000,
+    stdio: 'inherit'
 });
-
-scale.stdout.on( 'data', ( data ) => {
-    console.log( data )
-})
