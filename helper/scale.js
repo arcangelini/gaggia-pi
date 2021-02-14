354e23b3d24ac576ad867e3ff81dd21af6352fb2
Tony Arcangelini <tony@arcangelini.com>
2021-02-14 12:08:48 +0100

    Add scale.js

--- helper/scale.js
+++ helper/scale.js
@@ -1,7 +1,7 @@
 /**
  * Process scale data
  */
-const spawn = require('child_process').spawn;
+const spawn = require('child_process').spawnSync;
 const setWeight = process.argv[2]
 
 const scale = spawn( 'python', [ '/home/pi/gaggia/helper/hx711py/scale.py', setWeight ], {
