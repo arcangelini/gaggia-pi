aa81ef6d7f061bbdfc6735f2abef04483f27be21
arcangelini <tony@arcangelini.com>
2021-02-14 11:07:12 +0000

    Update scale

--- helper/scale.js
+++ helper/scale.js
@@ -2,7 +2,7 @@
  * Process scale data
  */
 const spawn = require('child_process').spawn;
-const setWeight = process.argv[0]
+const setWeight = process.argv[2]
 
 const scale = spawn( 'python', [ '/home/pi/gaggia/helper/hx711py/scale.py', setWeight ], {
     timeout: 10000,
