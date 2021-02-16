e67f52bf804d6018d3adb948bc0a1ae140e248fd
Tony Arcangelini <tony@arcangelini.com>
2021-02-16 17:36:29 +0100

    Reading tweak

--- server.js
+++ server.js
@@ -37,10 +37,9 @@ io.on( 'connection', ( client ) => {
     console.log( 'SOCKET: ', 'A client connected', client.id );
     
     client.on( 'brew_start', ( setWeight ) => {
-        const scale = spawn( 'python3', [ '/home/pi/gaggia/helper/hx711py/scale.py', setWeight ], {
-            encoding: 'utf-8',
-            timeout: 10000,
-        });
+        const scale = spawn( 'python3', [ '/home/pi/gaggia/helper/hx711py/scale.py', setWeight ] );
+
+        scale.stdout.setEncoding( 'utf-8' )
 
         scale.stdout.on( 'data', data => {
             client.emit( 'brewing', data )
