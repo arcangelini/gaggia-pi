33e660e42f3e71954f9e9c306a38f657aa5ecca3
Tony Arcangelini <tony@arcangelini.com>
2021-02-23 15:19:34 +0100

    Clean scale.py

--- src/services/scale.py
+++ src/services/scale.py
@@ -3,56 +3,42 @@
 import time
 import sys
 sys.path.insert(0, '/home/pi/gaggia/hx711py/')
-print(sys.path)
 
-EMULATE_HX711=False
-
-referenceUnit = 1
-
-if not EMULATE_HX711:
-    import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
     from hx711 import HX711
-    GPIO.setwarnings( False )
-
-else:
-    from emulated_hx711 import HX711
+GPIO.setwarnings( False )
 
 def cleanAndExit():
-    print( "Cleaning..." )
-
-    if not EMULATE_HX711:
-        GPIO.cleanup()
-        
-    print("Bye!")
+    GPIO.cleanup()
     sys.exit()
 
-hx = HX711( 6, 5 )
-hx.set_reading_format( "MSB", "MSB" )
+# Setup scale 1
+hxA = HX711( 6, 5 )
+hxA.set_reading_format( "MSB", "MSB" )
+hxA.set_reference_unit( 6943 )
+hxA.reset()
+hxA.tare()
 
-hx.set_reference_unit( 6943 )
-hx.reset()
-hx.tare()
+# Setup scale 2
 
 current_weight = 0
 target_weight = int( sys.argv[1] )
+tic = time.perf_counter()
 
 print( "Filling to: " + str( target_weight ) )
-tic = time.perf_counter()
 
 while current_weight < target_weight:
     try:
-        current_weight = hx.get_weight( 3 )
-
+        current_weight = hxA.get_weight( 1 ) #+ hxB.get_weight( 1 )
         if current_weight < 0.009:
             current_weight = 0.000
 
         toc = time.perf_counter()
         print( '{0:.2f}'.format( toc - tic ), ',', '{0:.2f}'.format( current_weight ) )
 
-        hx.reset()
+        hxA.reset()
 
         sys.stdout.flush()
 
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
-
